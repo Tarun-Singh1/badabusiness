f08378cbd6a80449982fe6007bca5b93cd79c10d
Tarun Singh <143503988+Tarun-Singh1@users.noreply.github.com>
2023-12-01 11:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function submitForm() {
+    // Your form processing logic goes here
+
+    // Redirect to WhatsApp link
+    window.location.href = "https://wa.me/919810544443?text=I%20want%20to%20know%20how%20Vivek%20Sir%20can%20solve%20my%20prob%20and%20help%20me%20grow.%20Referred%20by%20:%20BIMA001406";
+}
+
+
+function redirectToWhatsApp() {
+    // Create a URL with the WhatsApp link
+    var whatsappLink = "https://wa.me/919810544443?text=I%20want%20to%20know%20how%20Vivek%20Sir%20can%20solve%20my%20prob%20and%20help%20me%20grow.%20Referred%20by%20:%20BIMA001406";
+
+    // Redirect to the WhatsApp link
+    window.location.href = whatsappLink;
+}
